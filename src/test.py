04322e6660115019102afc4240d0077a03c98a05
tanglemontree <123568825@qq.com>
2016-11-12 21:23:24 +0800

    annotate some code for test git commit in pycharm

--- src/test.py
+++ src/test.py
@@ -1,24 +1,24 @@
 #from log import DEBUG
 #from base import appdefine
-from base import mpath
-
-print(mpath.inistance().getroot())
-print(mpath.inistance().getlogpath())
-c = mpath.inistance().getconfigpath()
-c = 'c:1.txt'
-print(mpath.inistance().getconfigpath())
-from user import usermng,user
-
-cuser = user()
-cuser.name = 'tang'
-cuser.online = True
-usermng.instance().add(cuser)
-puser = cuser
-cuser.name = 'li'
-cuser.nickname = 'loe'
-usermng.instance().add(cuser)
-usermng.instance().writetofile()
-c = usermng.instance()
+# from base import mpath
+#
+# print(mpath.inistance().getroot())
+# print(mpath.inistance().getlogpath())
+# c = mpath.inistance().getconfigpath()
+# c = 'c:1.txt'
+# print(mpath.inistance().getconfigpath())
+# from user import usermng,user
+#
+# cuser = user()
+# cuser.name = 'tang'
+# cuser.online = True
+# usermng.instance().add(cuser)
+# puser = cuser
+# cuser.name = 'li'
+# cuser.nickname = 'loe'
+# usermng.instance().add(cuser)
+# usermng.instance().writetofile()
+# c = usermng.instance()
 
 #s = dir()
 #import sys
